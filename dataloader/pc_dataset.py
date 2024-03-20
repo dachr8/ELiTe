bc0c229e1a799282d8ada0de6afa055a751c7558
dachr <32724398+dachr8@users.noreply.github.com>
2024-03-20 14:54:06 +0800

    2DPASS

---
+++ dataloader/pc_dataset.py
@@ -0,0 +1,139 @@
+import os
+import yaml
+import numpy as np
+from PIL import Image
+from torch.utils import data
+
+REGISTERED_PC_DATASET_CLASSES = {}
+
+
+def register_dataset(cls, name=None):
+    global REGISTERED_PC_DATASET_CLASSES
+    if name is None:
+        name = cls.__name__
+    assert name not in REGISTERED_PC_DATASET_CLASSES, f"exist class: {REGISTERED_PC_DATASET_CLASSES}"
+    REGISTERED_PC_DATASET_CLASSES[name] = cls
+    return cls
+
+
+def get_pc_model_class(name):
+    global REGISTERED_PC_DATASET_CLASSES
+    assert name in REGISTERED_PC_DATASET_CLASSES, f"available class: {REGISTERED_PC_DATASET_CLASSES}"
+    return REGISTERED_PC_DATASET_CLASSES[name]
+
+
+def absoluteFilePaths(directory, num_vote):
+    for dirpath, _, filenames in os.walk(directory):
+        filenames.sort()
+        for f in filenames:
+            for _ in range(num_vote):
+                yield os.path.abspath(os.path.join(dirpath, f))
+
+
+@register_dataset
+class SemanticKITTI(data.Dataset):
+    def __init__(self, config, data_path, imageset='train', num_vote=1):
+        with open(config['dataset_params']['label_mapping'], 'r') as stream:
+            semkittiyaml = yaml.safe_load(stream)
+
+        self.config = config
+        self.num_vote = num_vote
+        self.learning_map = semkittiyaml['learning_map']
+        self.imageset = imageset
+
+        if imageset == 'train':
+            split = semkittiyaml['split']['train']
+            if config['train_params'].get('trainval', False):
+                split += semkittiyaml['split']['valid']
+        elif imageset == 'val':
+            split = semkittiyaml['split']['valid']
+        elif imageset == 'test':
+            split = semkittiyaml['split']['test']
+        else:
+            raise Exception('Split must be train/val/test')
+
+        self.im_idx = []
+        self.proj_matrix = {}
+
+        for i_folder in split:
+            self.im_idx += absoluteFilePaths('/'.join([data_path, str(i_folder).zfill(2), 'velodyne']), num_vote)
+            calib_path = os.path.join(data_path, str(i_folder).zfill(2), "calib.txt")
+            calib = self.read_calib(calib_path)
+            proj_matrix = np.matmul(calib["P2"], calib["Tr"])
+            self.proj_matrix[i_folder] = proj_matrix
+
+        seg_num_per_class = config['dataset_params']['seg_labelweights']
+        seg_labelweights = seg_num_per_class / np.sum(seg_num_per_class)
+        self.seg_labelweights = np.power(np.amax(seg_labelweights) / seg_labelweights, 1 / 3.0)
+
+    def __len__(self):
+        """Denotes the total number of samples"""
+        return len(self.im_idx)
+
+    @staticmethod
+    def read_calib(calib_path):
+        """
+        :param calib_path: Path to a calibration text file.
+        :return: dict with calibration matrices.
+        """
+        calib_all = {}
+        with open(calib_path, 'r') as f:
+            for line in f.readlines():
+                if line == '\n':
+                    break
+                key, value = line.split(':', 1)
+                calib_all[key] = np.array([float(x) for x in value.split()])
+
+        # reshape matrices
+        calib_out = {
+            'P2': calib_all['P2'].reshape(3, 4),  # 3x4 projection matrix for left camera
+            'Tr': np.identity(4)  # 4x4 matrix
+        }
+        calib_out['Tr'][:3, :4] = calib_all['Tr'].reshape(3, 4)
+
+        return calib_out
+
+    def __getitem__(self, index):
+        raw_data = np.fromfile(self.im_idx[index], dtype=np.float32).reshape((-1, 4))
+        origin_len = len(raw_data)
+        points = raw_data[:, :3]
+
+        if self.imageset == 'test':
+            annotated_data = np.expand_dims(np.zeros_like(raw_data[:, 0], dtype=int), axis=1)
+            instance_label = np.expand_dims(np.zeros_like(raw_data[:, 0], dtype=int), axis=1)
+        else:
+            annotated_data = np.fromfile(self.im_idx[index].replace('velodyne', 'labels')[:-3] + 'label',
+                                         dtype=np.uint32).reshape((-1, 1))
+            instance_label = annotated_data >> 16
+            annotated_data = annotated_data & 0xFFFF  # delete high 16 digits binary
+            annotated_data = np.vectorize(self.learning_map.__getitem__)(annotated_data)
+
+            if self.config['dataset_params']['ignore_label'] != 0:
+                annotated_data -= 1
+                annotated_data[annotated_data == -1] = self.config['dataset_params']['ignore_label']
+
+        image_file = self.im_idx[index].replace('velodyne', 'image_2').replace('.bin', '.png')
+        image = Image.open(image_file)
+        proj_matrix = self.proj_matrix[int(self.im_idx[index][-22:-20])]
+
+        data_dict = {
+            'xyz': points,
+            'labels': annotated_data.astype(np.uint8),
+            'instance_label': instance_label,
+            'signal': raw_data[:, 3:4],
+            'origin_len': origin_len,
+            'img': image,
+            'proj_matrix': proj_matrix
+        }
+
+        return data_dict, self.im_idx[index]
+
+
+def get_SemKITTI_label_name(label_mapping):
+    with open(label_mapping, 'r') as stream:
+        semkittiyaml = yaml.safe_load(stream)
+    SemKITTI_label_name = dict()
+    for i in sorted(list(semkittiyaml['learning_map'].keys()))[::-1]:
+        SemKITTI_label_name[semkittiyaml['learning_map'][i]] = semkittiyaml['labels'][i]
+
+    return SemKITTI_label_name
